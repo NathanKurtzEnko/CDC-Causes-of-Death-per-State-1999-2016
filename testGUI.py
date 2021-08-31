71c7bd45f95cde1f7a36a623d279ba121ab9b719
NathanKurtzEnko <gymnathan@swbell.net>
2021-08-30 20:21:57 -0500

    Added Docstrings, getDims() and sizeWin() methods

--- testGUI.py
+++ testGUI.py
@@ -1,28 +1,53 @@
+#Make sure to install the following modules/packages
+#py -m pip install pyqt5
+
 #import packages and modules
 from PyQt5 import QtWidgets as qt
-
+import sys
 
 #define window class
 class myUI:
     """This is a test UI for other projects
     """
     def __init__(self):
-        self.app = qt.QApplication([])
-        self.label = qt.QLabel('Hello world')
-        self.window = qt.QWidget()
+        """This initializes our window/UI and creates some useful pieces for other operations
+        """
+        self.app = qt.QApplication(sys.argv)
+        self.window = qt.QMainWindow()
+        self.screenSize = qt.QDesktopWidget().screenGeometry(-1)
+        self.window.setGeometry(self.getDims()[1]/4, self.getDims()[0]/4, self.getDims()[1]/2, self.getDims()[0]/2)
 
     def createApp(self):
+        """This renders our window
+        """
         app = self.app
         window = self.window
         window.show()
         app.exec()
 
     def winTitle(self, title):
+        """This allows us to change the title of our window
+
+        Args:
+            title (string): This argument is what our new window title will be set to
+        """
         winTitle = title
         window = self.window
         window.setWindowTitle(winTitle)
 
+    def getDims(self):
+        """This allows returns the dimensions of the user window
 
+        Returns:
+            list: the list returned contains integer values of the screen resolution height and width
+        """
+        size = self.screenSize
+        height = int(size.height())
+        width = int(size.width())
+        return height, width
+
+    def sizeWin(self, width, height):
+        self.window.setGeometry(self.getDims()[1]/4, self.getDims()[1]/4, width, height)
 
 #create object
 test = myUI()
@@ -32,3 +57,6 @@ test.winTitle("Test")
 
 #launch the window
 test.createApp()
+
+#testing stuff
+#print(test.getDims())
